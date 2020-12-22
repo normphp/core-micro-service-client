9e42f042c17442b2eedc1fb62e31542c15231df4
pizepei <pizepei@pizepei.com>
2020-12-22 11:46:13 +0800

    维护配置

--- src/MicroClient.php
+++ src/MicroClient.php
@@ -14,10 +14,15 @@ class MicroClient
      */
     protected static $clientObj = [];
     /**
-     * 配置
+     * app配置
      * @var array
      */
-    private $config = [];
+    private $appConfig = [];
+    /**
+     * 核心配置
+     * @var array
+     */
+    private $coreConfig = [];
     /**
      * 缓存
      * @var \Redis
@@ -28,63 +33,66 @@ class MicroClient
      * @var string
      */
     private $action = '';
+
     /**
      * 对应每一个app应用实例化一个对象
      * MicroClient constructor.
      * @param \Redis $redis
+     * @param array $coreConfig
      * @param array $config
      */
-    public function __construct(\Redis $redis, array $config)
+    public function __construct(\Redis $redis,array $coreConfig,array $appConfig)
     {
+        # 核心配置
+        $coreConfig = $coreConfig;
         # 基础配置
-        $this->config = $config;
-        # 缓存配置
+        $this->appConfig = $appConfig;
+        # 缓存对象
         $this->redis = $redis;
-
     }
     /**
      * @Author 皮泽培
      * @Created 2019/10/21 11:06
      * @param \Redis $redis
      * @param array $config
-     * @param string $action  行为配置标识
+     * @param string $action
      * @return MicroClient
      * @throws \Exception
      * @title  路由标题
      * @explain 路由功能说明
      */
-    public static function init(\Redis $redis, array $config)
+    public static function init(\Redis $redis, array $coreConfig,array $appConfig)
     {
-        if ($config ==[] || !isset($config['CONFIG']['appid'])){
-            throw new  \Exception('config error');
-        }
+        if ($coreConfig ==[] || !isset($coreConfig['appid'])){throw new  \Exception('config error');}
+        if ($appConfig ==[] || !isset($appConfig['appid'])){throw new  \Exception('app config error');}
+
         # 判断对应应用的客户端是否已经实例化
-        if (!isset(static::$clientObj[$config['CONFIG']['appid']])){
-            static::$clientObj[$config['CONFIG']['appid']] = new static($redis,$config);
+        if (!isset(static::$clientObj[$appConfig['appid']])){
+            static::$clientObj[$appConfig['appid']] = new static($redis,$coreConfig,$appConfig);
         }
         # 返回实例化对象
-        return static::$clientObj[$config['CONFIG']['appid']];
+        return static::$clientObj[$appConfig['appid']];
     }
 
     /**
      * 请求接口
-     * @param $param
+     * @param array $param 请求数据
      * @return array
      * @throws \Exception
      */
-    public function send(array $param,string $action)
+    public function send(array $param)
     {
         # 当前使用的服务
         $this->action = $action;
         # 设置 configId
-        $param['configId'] = $this->config[$this->action]['configId']??'';
+        $param['configId'] = $this->appConfig['configId']??'';
         # url
-        $url = $this->config[$this->action]['url'].$this->config[$this->action]['api'].$this->config['CONFIG']['appid'].'.json';
+        $url = $this->appConfig['url'].$this->appConfig['api'].$this->coreConfig['appid'].'.json';
         # 确定数据
-        $Prpcrypt = new  Prpcrypt($this->config['CONFIG']['encodingAesKey']);
+        $Prpcrypt = new  Prpcrypt($this->coreConfig['encodingAesKey']);
 
-        $data = $Prpcrypt->yieldCiphertext(Helper()->json_encode($param),$this->config['CONFIG']['appid'],$this->config['CONFIG']['token']);
-        $res = Helper()->httpRequest($url,Helper()->json_encode($data),empty($this->config[$this->action]['hostDomain'])?[]:['header'=>['Host:'.$this->config[$this->action]['hostDomain']],'ssl'=>0]);
+        $data = $Prpcrypt->yieldCiphertext(Helper()->json_encode($param),$this->coreConfig['appid'],$this->coreConfig['token']);
+        $res = Helper()->httpRequest($url,Helper()->json_encode($data),empty($this->appConfig['hostDomain'])?[]:['header'=>['Host:'.$this->appConfig['hostDomain']],'ssl'=>0]);
         if ($res['code'] !== 200){throw new \Exception('httpRequest error  '.$res['code']);}
         $body = Helper()->json_decode($res['body']);
         if (empty($body)){throw new \Exception('body empty '.$res['body']);}
